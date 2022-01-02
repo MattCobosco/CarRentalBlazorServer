2b60905c488db5413bcf303a0e64fa7b433abe83
MattCobosco <mateuszkalucki@gmail.com>
2022-01-02 18:09:00 +0100

    added API controller showing all Model Vehicles

---
+++ WebApp/Controllers/GetAllVehicleModelsController.cs
@@ -0,0 +1,27 @@
+﻿using CoreBusiness;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using UseCases.DataStorePluginInterfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllVehicleModelsController : ControllerBase
+    {
+        private readonly IVehicleModelRepository _vehicleModelRepository;
+
+        public GetAllVehicleModelsController(IVehicleModelRepository vehicleModelRepository)
+        {
+            _vehicleModelRepository = vehicleModelRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<VehicleModel> ShowAllVehicleModelsDetails()
+        {
+            return _vehicleModelRepository.GetVehicleModels();
+        }
+    }
+}
