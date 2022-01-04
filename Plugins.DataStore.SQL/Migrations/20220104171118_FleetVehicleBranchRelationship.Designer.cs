4cd576757936f1950085088ebb139c97ae616373
MattCobosco <mateuszkalucki@gmail.com>
2022-01-04 19:10:52 +0100

    added Relationship between Fleet Vehicle Table and Branch Table

--- Plugins.DataStore.SQL/Migrations/20220104171118_FleetVehicleBranchRelationship.Designer.cs
+++ Plugins.DataStore.SQL/Migrations/20220104171118_FleetVehicleBranchRelationship.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Plugins.DataStore.SQL;
 
 namespace Plugins.DataStore.SQL.Migrations
 {
     [DbContext(typeof(CarRentalContext))]
-    partial class CarRentalContextModelSnapshot : ModelSnapshot
+    [Migration("20220104171118_FleetVehicleBranchRelationship")]
+    partial class FleetVehicleBranchRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Plugins.DataStore.SQL/Migrations/20220104171118_FleetVehicleBranchRelationship.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Plugins.DataStore.SQL.Migrations
+{
+    public partial class FleetVehicleBranchRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Branches",
+                columns: new[] { "BranchId", "Address", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Szklary 138 80-835 Gdańsk", null, "Gdańsk" },
+                    { 2, "Rozłogi 1 01-323 Warszawa", null, "Warszawa" },
+                    { 3, "Olszanicka 174 30-241 Kraków", null, "Kraków - Airport" },
+                    { 4, "Conrada 63 31-357 Kraków", null, "Kraków - City" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FleetVehicles_CurrentBranchId",
+                table: "FleetVehicles",
+                column: "CurrentBranchId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FleetVehicles_Branches_CurrentBranchId",
+                table: "FleetVehicles",
+                column: "CurrentBranchId",
+                principalTable: "Branches",
+                principalColumn: "BranchId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FleetVehicles_Branches_CurrentBranchId",
+                table: "FleetVehicles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FleetVehicles_CurrentBranchId",
+                table: "FleetVehicles");
+
+            migrationBuilder.DeleteData(
+                table: "Branches",
+                keyColumn: "BranchId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Branches",
+                keyColumn: "BranchId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Branches",
+                keyColumn: "BranchId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Branches",
+                keyColumn: "BranchId",
+                keyValue: 4);
+        }
+    }
+}
