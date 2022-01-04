e2353d30dc3486656bee2c53ac8901807c4c7459
MattCobosco <mateuszkalucki@gmail.com>
2022-01-04 12:45:32 +0100

    added HTTPGet to list all Branches

---
+++ WebApp/Controllers/GetAllBranchesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using CoreBusiness;
+using UseCases.DataStorePluginInterfaces;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllBranchesController : ControllerBase
+    {
+        private readonly IBranchRepository _branchRepository;
+
+        public GetAllBranchesController(IBranchRepository branchRepository)
+        {
+            _branchRepository = branchRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Branch> ShowAllBranchesDetails()
+        {
+            return _branchRepository.GetBranches();
+        }
+    }
+}
