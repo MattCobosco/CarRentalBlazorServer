76fee7f6a81bee387bffd20fab293fbd46eb6688
MattCobosco <mateuszkalucki@gmail.com>
2022-01-15 15:04:15 +0100

    added Contact Details to Employee model

--- CoreBusiness/Employee.cs
+++ CoreBusiness/Employee.cs
@@ -12,6 +12,7 @@ namespace CoreBusiness
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
+        public string ContactDetails { get; set; }
         public int BranchId { get; set; }
 
         // Navigation properties for EF Core
---
+++ Plugins.DataStore.SQL/Migrations/20220115134716_AddedContactDetailsToEmployee.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Plugins.DataStore.SQL.Migrations
+{
+    public partial class AddedContactDetailsToEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactDetails",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "GD19791",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2022, 1, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2023, 1, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "GD23372",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 1, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "GD38400",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 10, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 1, 29, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR14805",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2022, 1, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2023, 1, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR14819",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 1, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR17430",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 10, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 1, 29, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR19676",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2022, 1, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2023, 1, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR27495",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 1, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR37442",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 10, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 5, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "WW21759",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 1, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "WW51732",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 10, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 1, 29, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "WW81713",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2022, 1, 15, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2023, 1, 15, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactDetails",
+                table: "Employees");
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "GD19791",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2022, 1, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2023, 1, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "GD23372",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 1, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "GD38400",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 10, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 1, 28, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR14805",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2022, 1, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2023, 1, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR14819",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 1, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR17430",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 10, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 1, 28, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR19676",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2022, 1, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2023, 1, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR27495",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 1, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "KR37442",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 10, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 4, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "WW21759",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 1, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "WW51732",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2021, 10, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 1, 28, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "FleetVehicles",
+                keyColumn: "FleetVehicleLicensePlate",
+                keyValue: "WW81713",
+                columns: new[] { "DateAdded", "MaintenanceDate" },
+                values: new object[] { new DateTime(2022, 1, 14, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2023, 1, 14, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+    }
+}
